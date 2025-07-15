829fdd07dd16bfdd9088d83f351ffd80dc76c2ee
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-07-15 23:44:18 +0000

    Init

---
+++ src/test/java/com/safepassword/SafePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safepassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
